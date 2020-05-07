d434e21d7b6aa4cb7102ce370a3d33744859f934
PanosIosifidis <panos.iosifidis@yahoo.com>
2020-05-07 17:37:14 +0100

    Add project files.

---
+++ MessageSender01/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace MessageSender01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonSendHello = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelMain = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonConfirmCredentials = new System.Windows.Forms.Button();
+            this.buttonLoadPrevCredentials = new System.Windows.Forms.Button();
+            this.buttonConfirmAddress = new System.Windows.Forms.Button();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonLoadPrevAddress = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonSendContinuously = new System.Windows.Forms.Button();
+            this.buttonStopSending = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSendHello
+            // 
+            this.buttonSendHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSendHello.Location = new System.Drawing.Point(12, 256);
+            this.buttonSendHello.Name = "buttonSendHello";
+            this.buttonSendHello.Size = new System.Drawing.Size(260, 34);
+            this.buttonSendHello.TabIndex = 0;
+            this.buttonSendHello.Text = "Send \"Hello\" once";
+            this.buttonSendHello.UseVisualStyleBackColor = true;
+            this.buttonSendHello.Click += new System.EventHandler(this.buttonHello_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(39, 92);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(103, 20);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Device Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(148, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(12, 124);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(130, 20);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Device Password";
+            // 
+            // labelMain
+            // 
+            this.labelMain.AutoSize = true;
+            this.labelMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMain.Location = new System.Drawing.Point(12, 9);
+            this.labelMain.Name = "labelMain";
+            this.labelMain.Size = new System.Drawing.Size(539, 24);
+            this.labelMain.TabIndex = 5;
+            this.labelMain.Text = "Welcome to Message Sender. Please Enter Device Credentials";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(148, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // buttonConfirmCredentials
+            // 
+            this.buttonConfirmCredentials.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConfirmCredentials.Location = new System.Drawing.Point(12, 153);
+            this.buttonConfirmCredentials.Name = "buttonConfirmCredentials";
+            this.buttonConfirmCredentials.Size = new System.Drawing.Size(260, 37);
+            this.buttonConfirmCredentials.TabIndex = 7;
+            this.buttonConfirmCredentials.Text = "Confirm Credentials";
+            this.buttonConfirmCredentials.UseVisualStyleBackColor = true;
+            this.buttonConfirmCredentials.Click += new System.EventHandler(this.buttonConfirmCredentials_Click);
+            // 
+            // buttonLoadPrevCredentials
+            // 
+            this.buttonLoadPrevCredentials.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoadPrevCredentials.Location = new System.Drawing.Point(12, 55);
+            this.buttonLoadPrevCredentials.Name = "buttonLoadPrevCredentials";
+            this.buttonLoadPrevCredentials.Size = new System.Drawing.Size(260, 28);
+            this.buttonLoadPrevCredentials.TabIndex = 8;
+            this.buttonLoadPrevCredentials.Text = "Load Previous Credentials";
+            this.buttonLoadPrevCredentials.UseVisualStyleBackColor = true;
+            this.buttonLoadPrevCredentials.Click += new System.EventHandler(this.buttonLoadPrevCredentials_Click);
+            // 
+            // buttonConfirmAddress
+            // 
+            this.buttonConfirmAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConfirmAddress.Location = new System.Drawing.Point(279, 55);
+            this.buttonConfirmAddress.Name = "buttonConfirmAddress";
+            this.buttonConfirmAddress.Size = new System.Drawing.Size(342, 28);
+            this.buttonConfirmAddress.TabIndex = 9;
+            this.buttonConfirmAddress.Text = "Load Previous Address";
+            this.buttonConfirmAddress.UseVisualStyleBackColor = true;
+            this.buttonConfirmAddress.Click += new System.EventHandler(this.buttonLoadPrevAddress_Click);
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress.Location = new System.Drawing.Point(278, 92);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(153, 20);
+            this.labelAddress.TabIndex = 10;
+            this.labelAddress.Text = "Destination Address";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(437, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 26);
+            this.textBox3.TabIndex = 11;
+            // 
+            // buttonLoadPrevAddress
+            // 
+            this.buttonLoadPrevAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoadPrevAddress.Location = new System.Drawing.Point(278, 153);
+            this.buttonLoadPrevAddress.Name = "buttonLoadPrevAddress";
+            this.buttonLoadPrevAddress.Size = new System.Drawing.Size(343, 37);
+            this.buttonLoadPrevAddress.TabIndex = 12;
+            this.buttonLoadPrevAddress.Text = "Confirm Address";
+            this.buttonLoadPrevAddress.UseVisualStyleBackColor = true;
+            this.buttonLoadPrevAddress.Click += new System.EventHandler(this.buttonConfirmsAddress_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(209, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Send Custome Message:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(278, 353);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(343, 26);
+            this.textBox4.TabIndex = 14;
+            // 
+            // buttonSendContinuously
+            // 
+            this.buttonSendContinuously.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSendContinuously.Location = new System.Drawing.Point(12, 296);
+            this.buttonSendContinuously.Name = "buttonSendContinuously";
+            this.buttonSendContinuously.Size = new System.Drawing.Size(419, 34);
+            this.buttonSendContinuously.TabIndex = 15;
+            this.buttonSendContinuously.Text = "Send Time and Date continuously";
+            this.buttonSendContinuously.UseVisualStyleBackColor = true;
+            this.buttonSendContinuously.Click += new System.EventHandler(this.buttonSendContinuously_Click);
+            // 
+            // buttonStopSending
+            // 
+            this.buttonStopSending.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStopSending.Location = new System.Drawing.Point(437, 296);
+            this.buttonStopSending.Name = "buttonStopSending";
+            this.buttonStopSending.Size = new System.Drawing.Size(184, 34);
+            this.buttonStopSending.TabIndex = 16;
+            this.buttonStopSending.Text = "Stop Sending";
+            this.buttonStopSending.UseVisualStyleBackColor = true;
+            this.buttonStopSending.Click += new System.EventHandler(this.buttonStopSending_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(437, 385);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(184, 34);
+            this.buttonSend.TabIndex = 17;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 434);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonStopSending);
+            this.Controls.Add(this.buttonSendContinuously);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonLoadPrevAddress);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.buttonConfirmAddress);
+            this.Controls.Add(this.buttonLoadPrevCredentials);
+            this.Controls.Add(this.buttonConfirmCredentials);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelMain);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonSendHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSendHello;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelMain;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonConfirmCredentials;
+        private System.Windows.Forms.Button buttonLoadPrevCredentials;
+        private System.Windows.Forms.Button buttonConfirmAddress;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonLoadPrevAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonSendContinuously;
+        private System.Windows.Forms.Button buttonStopSending;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ MessageSender01/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Win32;
+using MessageSender01.Properties;
+
+namespace MessageSender01
+{
+    public partial class Form1 : Form
+    {
+        static HttpClient client = new HttpClient();
+        string previousAddress = Settings.Default.previousAddress;
+        string previousDevName = Settings.Default.previousName;
+        string previousPasword = Settings.Default.previousPassword;
+        bool credentialsOK = false;
+        bool addressOK = false;
+        DateTime currentGMT;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonSendHello.Enabled = false;
+            buttonSendContinuously.Enabled = false;
+            buttonStopSending.Enabled = false;
+            buttonSend.Enabled = false;
+
+        }
+
+
+
+
+        public class InputData
+        {
+            public string Message { get; set; }
+        }
+
+        static async Task<Uri> CreateMessageAsync(InputData testData)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/values/", testData);
+            response.EnsureSuccessStatusCode();
+            return response.Headers.Location;
+        }
+
+
+        static async Task RunAsync(string inputmessage)
+        {
+
+            try
+            {
+                InputData data000 = new InputData();
+                data000.Message = inputmessage;
+                Console.WriteLine("Message = " + data000.Message);
+
+                var url =  CreateMessageAsync(data000);
+                Console.WriteLine("Message successfully sent!");
+                return;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return;
+        }
+
+        private void buttonLoadPrevCredentials_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = previousDevName;
+            textBox2.Text = previousPasword;
+            credentialsOK = true;
+            enable_lower_interface();
+        }
+
+        private void buttonLoadPrevAddress_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = previousAddress;
+            credentialsOK = true;
+            enable_lower_interface();
+        }
+
+        private void buttonConfirmCredentials_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("~"))
+            {
+                labelMain.Text = "Username Cannot contain '~' ";
+                return;
+            }
+            if (textBox2.Text.Contains("~"))
+            {
+                labelMain.Text = "Password Cannot contain '~' ";
+                return;
+            }
+            if (textBox1.Text.Length > 5 && textBox2.Text.Length > 5)
+            {
+                Settings.Default.previousName = textBox1.Text;
+                Settings.Default.previousPassword = textBox2.Text;
+                Settings.Default.Save();
+                credentialsOK = true;
+                enable_lower_interface();
+            }
+            else labelMain.Text = "Please enter more appropriate credentials";
+        }
+
+        private void buttonConfirmsAddress_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length > 5)
+            {
+                Settings.Default.previousAddress = textBox3.Text;
+                Settings.Default.Save();
+                addressOK = true;
+                enable_lower_interface();
+            }
+            else labelMain.Text = "Please enter an appropriate address";
+        }
+
+        private void enable_lower_interface()
+        {
+            if(credentialsOK && addressOK)
+            {
+                client.BaseAddress = new Uri(textBox3.Text); //"http://localhost:55461/"
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                labelMain.Text = "Device Ready to Launch";
+                buttonConfirmCredentials.Enabled = false;
+                buttonLoadPrevCredentials.Enabled = false;
+                buttonConfirmAddress.Enabled = false;
+                buttonLoadPrevAddress.Enabled = false;
+                buttonSendHello.Enabled = true;
+                buttonSendContinuously.Enabled = true;
+                buttonStopSending.Enabled = true;
+                buttonSend.Enabled = true;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void buttonHello_Click(object sender, EventArgs e)
+        {
+            string message_to_send = textBox1.Text + "~" + textBox2.Text + "~" + "Hello" ;
+            RunAsync(message_to_send); //.GetAwaiter().GetResult();
+            return;
+        }
+
+        private void buttonSendContinuously_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void buttonStopSending_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentGMT = DateTime.Now;
+            string message_to_send = textBox1.Text + "~" + textBox2.Text + "~" + currentGMT;
+            RunAsync(message_to_send);
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string message_to_send = textBox1.Text + "~" + textBox2.Text + "~" + textBox4.Text;
+            RunAsync(message_to_send);
+        }
+    }
+}
